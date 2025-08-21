f4a07cc02cbaf9865fc8e2171e1d2e543d06ce38
niiccoo2 <nicosmith873@gmail.com>
2025-08-21 07:49:34 -0400

    fix: Add extra ? to the values part of add_data

--- server/add_data.py
+++ server/add_data.py
@@ -3,10 +3,10 @@ import sqlite3
 conn = sqlite3.connect("food_weights.db")
 c = conn.cursor()
 
-c.execute("INSERT INTO weights (group_id, name, weight) VALUES (?, ?)", ("troop30", "Nico", 5))
-c.execute("INSERT INTO weights (group_id, name, weight) VALUES (?, ?)", ("troop30", "Ben", 7))
-c.execute("INSERT INTO weights (group_id, name, weight) VALUES (?, ?)", ("whs", "Bob", 12))
-c.execute("INSERT INTO weights (group_id, name, weight) VALUES (?, ?)", ("whs", "Mary", 12))
+c.execute("INSERT INTO weights (group_id, name, weight) VALUES (?, ?, ?)", ("troop30", "Nico", 5))
+c.execute("INSERT INTO weights (group_id, name, weight) VALUES (?, ?, ?)", ("troop30", "Ben", 7))
+c.execute("INSERT INTO weights (group_id, name, weight) VALUES (?, ?, ?)", ("whs", "Bob", 12))
+c.execute("INSERT INTO weights (group_id, name, weight) VALUES (?, ?, ?)", ("whs", "Mary", 12))
 
 conn.commit()
 conn.close()

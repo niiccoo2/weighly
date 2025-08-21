dfb35218a617374eadaf8de4de4927a6b4bf2ee8
niiccoo2 <nicosmith873@gmail.com>
2025-08-21 07:31:36 -0400

    Add check to see if the DB is there, if not it will create an empty one

--- server/main.py
+++ server/main.py
@@ -2,17 +2,44 @@ from fastapi import FastAPI
 from typing import List
 from pydantic import BaseModel
 import sqlite3
+import os
 
 app = FastAPI()
+db_file = "food_weights.db"
 
 class Weight(BaseModel):
     id: int
     name: str
     weight: float
 
+def createEmptyDB(name):
+    conn = sqlite3.connect(name)
+    c = conn.cursor()
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS weights (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        group_id TEXT NOT NULL,
+        name TEXT NOT NULL,
+        weight INTEGER NOT NULL
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+try: # Testing to see if there is a db, if there is not it will make one
+    if not os.path.exists(db_file):
+        createEmptyDB(db_file)
+        # raise FileNotFoundError("Database file does not exist.")
+    conn = sqlite3.connect(db_file)
+except (sqlite3.OperationalError, FileNotFoundError) as e:
+    print(f"Error: {e}")
+
+
 @app.get("/weights", response_model=List[Weight])
 def read_weights():
-    conn = sqlite3.connect("food_weights.db")
+    # conn = sqlite3.connect("food_weights.db")
     c = conn.cursor()
 
     c.execute("SELECT * FROM weights")

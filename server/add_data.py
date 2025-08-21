7f15c7b3d26d10841179e3c43ca29751c1c7d401
niiccoo2 <nicosmith873@gmail.com>
2025-08-21 07:35:39 -0400

    Change data in add_data.py to have group_id's

--- server/add_data.py
+++ server/add_data.py
@@ -3,8 +3,10 @@ import sqlite3
 conn = sqlite3.connect("food_weights.db")
 c = conn.cursor()
 
-c.execute("INSERT INTO weights (name, weight) VALUES (?, ?)", ("Nico", 5))
-c.execute("INSERT INTO weights (name, weight) VALUES (?, ?)", ("Ben", 7))
+c.execute("INSERT INTO weights (group_id, name, weight) VALUES (?, ?)", ("troop30", "Nico", 5))
+c.execute("INSERT INTO weights (group_id, name, weight) VALUES (?, ?)", ("troop30", "Ben", 7))
+c.execute("INSERT INTO weights (group_id, name, weight) VALUES (?, ?)", ("whs", "Bob", 12))
+c.execute("INSERT INTO weights (group_id, name, weight) VALUES (?, ?)", ("whs", "Mary", 12))
 
 conn.commit()
 conn.close()

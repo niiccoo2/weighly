3afee9ad08415a9dff2dd8aaf0cf1b2b188a19db
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-08-23 16:59:36 -0400

    Refactor leaderboard data structure and add support for kg mode

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,36 +1,76 @@
-<script>
-	import leaderboard from '../example.json';
+<script lang="ts">
+  import { page } from '$app/stores';
+  import { onMount } from 'svelte';
+
+  let leaderboard: any[] = [];
+  let kg_mode: string = "lbs"
+  let op_kg_mode: string = "kgs"
+  $: kgMode = $page.url.searchParams.has('kgs');
+
+  onMount(async () => {
+    if (kgMode) {
+      const module = await import('../example-kg.json');
+      leaderboard = module.default;
+	  kg_mode = "kgs"
+	  op_kg_mode = "lbs"
+    } else {
+      const module = await import('../example.json');
+      leaderboard = module.default;
+    }
+  });
 </script>
+
+<style>
+  /* .bevel-box {
+    padding: 1.5rem;
+    background: #e0e0e0;
+    border-radius: 12px;
+    box-shadow: inset 4px 4px 8px #b8b8b8,
+                inset -4px -4px 8px #ffffff;
+    font-family: sans-serif;
+    text-align: center;
+  } */
+  .lightmode-background {
+	background-color: #F5F5F7;
+	/* background-color: aqua; */
+  }
+  .lightmode-foreground {
+	background-color: #ffffff;
+  }
+
+</style>
 
 <svelte:head>
 	<title>Weighly - Troop 30 Leaderboard</title>
 </svelte:head>
 
-<div class="">
-	<div class="bg-gray-500 w-full p-4 shadow-lg">
+<div class="lightmode-background" style="width: 100vw; height: 100vh;">
+	<div class="w-full p-4">
 		<!-- Title -->
-		<p class="text-center text-7xl shadow-lg text-white">All Time Leaderboard</p>
+		<p class="text-center text-7xl" style="color: #99ccff; -webkit-text-stroke: 2px black; font-weight: 1000;">
+			Troop 30 - All Time Leaderboard
+		</p>
 	</div>
 	<div class="p-4 w-full max-w-4xl mx-auto text-center text-2xl">
 		<!-- Leaderboard -->
 
-		<table class="mx-auto">
+		<table class="mx-auto border-separate border-spacing-y-4">
 			<thead>
-				<tr class="">
-					<th>Rank</th>
-					<th>Player</th>
-					<th>Type</th>
-					<th>Score</th>
+				<tr>
+					<th></th>
+					<th></th>
+					<th></th>
+					<th></th>
 				</tr>
 			</thead>
 
 			<tbody>
 				{#each leaderboard.slice(0, 10) as item}
-					<tr>
-						<td class="px-4 py-2">{item.rank}</td>
+					<tr class="p-6 rounded-xl bg-gray-200 rounded-xl hover:shadow-[4px_4px_8px_#b8b8b8,-4px_-4px_8px_#ffffff] shadow-[2px_2px_4px_#b8b8b8,-2px_-2px_4px_#ffffff] text-center font-semibold hover:scale-105 lightmode-foreground">
+						<td class="px-4 py-2">{item.rank}.</td>
 						<td class="px-4 py-2">{item.name}</td>
-						<td class="px-4 py-2">{item.category}</td>
-						<td class="px-4 py-2">{item.score_lbs}</td>
+						<td class="px-2 py-2"  style="color: grey; font-size: 1rem;">{item.category}</td>
+						<td class="px-4 py-2">{item.score_lbs} <a href="?{op_kg_mode}">{kg_mode}</a></td>
 					</tr>
 				{/each}
 			</tbody>

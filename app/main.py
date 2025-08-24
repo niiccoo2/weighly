40a18623b17c3be7db787e183a101f78a2269265
Nico Smith <web@niiccoo2.xyz>
2025-08-24 18:55:10 -0400

    Refactored get_serial into its own file

--- app/main.py
+++ app/main.py
@@ -1,20 +1,21 @@
 import tkinter as tk
-import random
 import datetime
 import customtkinter
-from CTkMessagebox import CTkMessagebox 
-import serial
+from CTkMessagebox import CTkMessagebox
+from scale_utils import get_serial, get_serial_dummy
 
+use_dummy_scale = False # Remove this once the ability to enter weight is added
 
-#SERIALPORT = "/dev/ttyUSB0"  #Real Sparfun Open Scale
+if use_dummy_scale:
+    get_serial = get_serial_dummy
+
+SERIALPORT = "/dev/ttyUSB0"  #Real Sparfun Open Scale
 #SERIALPORT = "/dev/ttyACM0"  #Dummy Sparfun Open Scale on Arduino
-
 BAUDRATE = 9600
+FILENAME="./weighly_backup.csv"
 
-filename="./weighly_backup.csv"
-#ser = serial.Serial(SERIALPORT, BAUDRATE, timeout =1) #Real scale
 
-open(filename, "a")
+open(FILENAME, "a")
 
 ctk = customtkinter.CTk()
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
@@ -38,71 +39,12 @@ weight_to_display = tk.StringVar(ctk)
 ScoutType = tk.StringVar(ctk)
 ScoutType.set("Scout")
 Bigtotal = tk.StringVar(ctk)
-with open(filename, "r") as file:
+with open(FILENAME, "r") as file:
     file=file.read().split(",")
-    # print(file[-3])
-    # exit()
     try:
         Bigtotal.set(str(file[-3]))
     except:
         Bigtotal.set("0")
-
-
-# def get_serial(StringToSend):
-#     print("StringToSend = "+StringToSend)
-#     weight_string = ""
-#     weight = 00.0
-#     miliseconds = 0
-#     Data_Ready = 0
-    
-#     ser.reset_input_buffer()
-#     ser.reset_output_buffer()
-    
-#     #ser.write(bytearray("W",'ascii'))
-#     ser.write(StringToSend.encode('utf-8'))
-#     i=0
-#     #time.sleep(.1)
-#     while (Data_Ready == 0):
-#         Data_Ready = ser.inWaiting()
-#         i=i+1
-#         if i>10:
-#             print("I BROKE!")
-#             break
-    
-#     input_string =""
-    
-#     input_string = ser.readline().decode('utf-8')
-#     print (input_string)
-    
-#     try:
-#         split_input_string = input_string.split(",")
-#         print("weight splitting")
-#         print (split_input_string[0])
-#         print (split_input_string[1])
-#         weight_string = split_input_string[0]
-#         weight_string = weight_string.rstrip()
-#         weight_string = weight_string.rstrip('lbs')
-        
-#         print("weight_string = "+ weight_string)
-#         weight = float(weight_string)
-        
-#         print("weight = "+ str(weight))
-        
-#         #miliseconds_string = split_input_string[0].strip()
-#         #miliseconds = int(miliseconds_string)
-        
-#         print("milliseconds = " + str(miliseconds))
-              
-#         return weight
-        
-#     except:
-#         print("Not Weight Data")
-#         print (split_input_string[0])
-
-# Simulated scale data for testing
-def get_serial(StringToSend):  # Dummy scale
-    weight = random.randint(0, 200)
-    return weight
 
 
 # Write donation data to a file
@@ -112,7 +54,7 @@ def write_to_file():
     weight_to_file = weight_to_display.get().rstrip(" lbs.")
 
     if ScoutName != "":
-        with open(filename, "a") as hs:
+        with open(FILENAME, "a") as hs:
             ct = datetime.datetime.now()
             bt = float(Bigtotal.get())
             bt = round(bt + float(weight_to_file), 2)
@@ -133,11 +75,11 @@ def write_to_file():
 
 # Tare function placeholder
 def Tare_The_Scale():
-    Tare = get_serial("x") #gives error with dummy weight
+    Tare = get_serial(SERIALPORT, BAUDRATE, "x") #gives error with dummy weight
     
-    Tare = get_serial("1")
+    Tare = get_serial(SERIALPORT, BAUDRATE, "1")
     
-    Tare = get_serial("x")
+    Tare = get_serial(SERIALPORT, BAUDRATE, "x")
 
 
 # Buttons and labels
@@ -210,7 +152,7 @@ last_weight = None  # Store the last weight
 
 def my_mainloop():
     global last_weight
-    weight = get_serial("W")
+    weight = get_serial(SERIALPORT, BAUDRATE, "W")
     if weight != last_weight:
         last_weight = weight
         weight_to_display.set(f"{weight} lbs.")

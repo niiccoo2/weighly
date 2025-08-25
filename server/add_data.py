00eefd8bf3f3f7fa9725d2148c3c9e4680a4b8b5
Nico Smith <web@niiccoo2.xyz>
2025-08-25 16:18:35 -0400

    Add more data

--- server/add_data.py
+++ server/add_data.py
@@ -3,8 +3,13 @@ import sqlite3
 conn = sqlite3.connect("food_weights.db")
 c = conn.cursor()
 
+# troop30 id: 0
 c.execute("INSERT INTO weights (event_id, name, weight, type, time) VALUES (?, ?, ?, ?, ?)", (0, "Nico", 5, "scout", "2024-11-23 13:56:18.506589"))
+c.execute("INSERT INTO weights (event_id, name, weight, type, time) VALUES (?, ?, ?, ?, ?)", (0, "Nico", 3, "scout", "2024-11-23 13:56:18.506589"))
 c.execute("INSERT INTO weights (event_id, name, weight, type, time) VALUES (?, ?, ?, ?, ?)", (0, "Ben", 3, "cub", "2024-11-23 13:56:18.506589"))
+c.execute("INSERT INTO weights (event_id, name, weight, type, time) VALUES (?, ?, ?, ?, ?)", (0, "Ben", 3, "scout", "2024-11-23 13:56:18.506589"))
+
+# whs id: 1
 c.execute("INSERT INTO weights (event_id, name, weight, type, time) VALUES (?, ?, ?, ?, ?)", (1, "Evan", 5, "", "2024-11-23 13:56:18.506589"))
 c.execute("INSERT INTO weights (event_id, name, weight, type, time) VALUES (?, ?, ?, ?, ?)", (1, "Bob", 5, "", "2024-11-23 13:56:18.506589"))
 

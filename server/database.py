0a22b9d0425e6aba09124d1631761c69b5a63994
Nico Smith <web@niiccoo2.xyz>
2025-08-25 16:24:33 -0400

    Fix old var names in get_entries

--- server/database.py
+++ server/database.py
@@ -22,10 +22,10 @@ def createEmptyDB(name):
     conn.commit()
     conn.close()
 
-def get_entries(group: str):
+def get_entries(event: str):
     conn = get_connection()
     c = conn.cursor()
-    c.execute("SELECT id, name, weight, type, time FROM weights WHERE group_id = ?", (group,))
+    c.execute("SELECT id, name, weight, type, time FROM weights WHERE event_id = ?", (event,))
     rows = c.fetchall()
     conn.close()
     return [Weight(id=row[0], name=row[1], weight=row[2], type=row[3], time=row[4]) for row in rows]

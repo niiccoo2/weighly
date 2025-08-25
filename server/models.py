c4aa774305a0c2a740f8eebc8a740bf2f16a70d9
Nico Smith <web@niiccoo2.xyz>
2025-08-25 17:26:48 -0400

    Add summary GET method

--- server/models.py
+++ server/models.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import List, Optional
 
 class Weight(BaseModel):
     id: int
@@ -11,3 +12,12 @@ class Summed_Weight(BaseModel):
     name: str
     weight: float
     type: str
+
+class Event(BaseModel):
+    event_id: int
+    name: str
+    custom_url: Optional[str] = None
+
+class Summary(BaseModel):
+    event: Event
+    totals: List[Summed_Weight]

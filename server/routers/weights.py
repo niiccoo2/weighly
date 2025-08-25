cf1337d41993ae72a73d331f4595e78a4aead0cd
Nico Smith <web@niiccoo2.xyz>
2025-08-25 15:36:56 -0400

    Add type and time to get method

--- server/routers/weights.py
+++ server/routers/weights.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 from typing import List
 from models import Weight
-from database import get_weights_by_group
+from database import get_weights
 
 router = APIRouter()
 
 @router.get("/weights/{group}", response_model=List[Weight])
 def read_weights(group: str):
-    return get_weights_by_group(group)
+    return get_weights(group)

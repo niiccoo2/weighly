4473b5b0299295ce11224dcd3f9e24f5064f4845
Nico Smith <web@niiccoo2.xyz>
2025-08-24 20:14:42 -0400

    Rename bad var names

--- app/database_utils.py
+++ app/database_utils.py
@@ -2,17 +2,17 @@ import datetime
 from CTkMessagebox import CTkMessagebox
 
 # Write donation data to a file
-def write_to_file(filename, scout, ScoutType, weight_to_display, Bigtotal):
-    ScoutName = scout.get().strip().title()
-    ScoutTypeDisplay = ScoutType.get().strip()
-    weight_to_file = weight_to_display.get().rstrip(" lbs.")
+def write_to_file(filename, name, person_type, weight, running_total):
+    ScoutName = name.get().strip().title()
+    ScoutTypeDisplay = person_type.get().strip()
+    weight_to_file = weight.get().rstrip(" lbs.")
 
     if ScoutName != "":
         with open(filename, "a") as hs:
             ct = datetime.datetime.now()
-            bt = float(Bigtotal.get())
+            bt = float(running_total.get())
             bt = round(bt + float(weight_to_file), 2)
-            Bigtotal.set(str(bt))
+            running_total.set(str(bt))
 
             hs.write(f"{ScoutName},{ScoutTypeDisplay},{weight_to_file},lbs,{bt},lbs,{ct}\n")
 

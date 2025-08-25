9769f98a4bc005ff02adc574e10e4e26ccbeab3e
Nico Smith <web@niiccoo2.xyz>
2025-08-24 20:30:38 -0400

    Rename more bad vars

--- app/database_utils.py
+++ app/database_utils.py
@@ -3,21 +3,21 @@ from CTkMessagebox import CTkMessagebox
 
 # Write donation data to a file
 def write_to_file(filename, name, person_type, weight, running_total):
-    ScoutName = name.get().strip().title()
+    clean_name = name.get().strip().title()
     ScoutTypeDisplay = person_type.get().strip()
     weight_to_file = weight.get().rstrip(" lbs.")
 
-    if ScoutName != "":
+    if clean_name != "":
         with open(filename, "a") as hs:
             ct = datetime.datetime.now()
             bt = float(running_total.get())
             bt = round(bt + float(weight_to_file), 2)
             running_total.set(str(bt))
 
-            hs.write(f"{ScoutName},{ScoutTypeDisplay},{weight_to_file},lbs,{bt},lbs,{ct}\n")
+            hs.write(f"{clean_name},{ScoutTypeDisplay},{weight_to_file},lbs,{bt},lbs,{ct}\n")
 
         CTkMessagebox(
-            title="Saved", message=f"{ScoutName}, thank you for your {weight_to_file} lbs. donation!"
+            title="Saved", message=f"Saved {weight_to_file} lbs. from {clean_name}"
         )
     else:
         CTkMessagebox(

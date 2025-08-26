4ee34752a39f1eaafbb7fdc7ffbd819462f0e676
Nico Smith <web@niiccoo2.xyz>
2025-08-25 22:16:38 -0400

    Chane add_data to use None insted of "" for empty type

--- server/add_data.py
+++ server/add_data.py
@@ -17,8 +17,8 @@ c.execute("INSERT INTO weights (event_id, name, weight, type, time) VALUES (?, ?
 c.execute("INSERT INTO weights (event_id, name, weight, type, time) VALUES (?, ?, ?, ?, ?)", (1, "Ben", 3, "scout", "2024-11-23 13:56:18.506589"))
 
 # whs id: 1
-c.execute("INSERT INTO weights (event_id, name, weight, type, time) VALUES (?, ?, ?, ?, ?)", (2, "Evan", 5, "", "2024-11-23 13:56:18.506589"))
-c.execute("INSERT INTO weights (event_id, name, weight, type, time) VALUES (?, ?, ?, ?, ?)", (2, "Bob", 5, "", "2024-11-23 13:56:18.506589"))
+c.execute("INSERT INTO weights (event_id, name, weight, type, time) VALUES (?, ?, ?, ?, ?)", (2, "Evan", 5, None, "2024-11-23 13:56:18.506589"))
+c.execute("INSERT INTO weights (event_id, name, weight, type, time) VALUES (?, ?, ?, ?, ?)", (2, "Bob", 5, None, "2024-11-23 13:56:18.506589"))
 
 conn.commit()
 conn.close()

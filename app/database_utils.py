38e8a6368f68773a072d713b303209b36f20e06e
Nico Smith <web@niiccoo2.xyz>
2025-08-25 21:28:51 -0400

    Make app send to api

--- app/database_utils.py
+++ app/database_utils.py
@@ -1,20 +1,24 @@
 import datetime
 from CTkMessagebox import CTkMessagebox
+from datetime import datetime
+import requests
+
+FILENAME="./weighly_backup.csv"
 
 # Write donation data to a file
-def write_to_file(filename, name, person_type, weight, running_total):
-    clean_name = name.get().strip().title()
-    ScoutTypeDisplay = person_type.get().strip()
-    weight_to_file = weight.get().rstrip(" lbs.")
+def save_to_file(filename: str, name: str, weight: float, person_type: str):
+    """
+    Saves name, weight, and type to local CSV.
+    """
+    clean_name = name.strip().title()
+    ScoutTypeDisplay = person_type.strip()
+    weight_to_file = weight
 
     if clean_name != "":
         with open(filename, "a") as hs:
-            ct = datetime.datetime.now()
-            bt = float(running_total.get())
-            bt = round(bt + float(weight_to_file), 2)
-            running_total.set(str(bt))
+            current_time = datetime.now()
 
-            hs.write(f"{clean_name},{ScoutTypeDisplay},{weight_to_file},{bt},{ct}\n")
+            hs.write(f"{clean_name},{ScoutTypeDisplay},{weight_to_file},{current_time}\n")
 
         CTkMessagebox(
             title="Saved", message=f"Saved {weight_to_file} lbs. from {clean_name}"
@@ -25,3 +29,28 @@ def write_to_file(filename, name, person_type, weight, running_total):
             message=f"Please Name The {ScoutTypeDisplay}",
             icon="cancel",
         )
+
+def save_to_database(name: str, weight: float, person_type: str):
+    """
+    Saves name, weight, and type to weighly backend.
+    """
+    url = "http://127.0.0.1:8000/1/add_weight" # This is for local testing
+
+    payload = {
+    "name": name,
+    "weight": weight,
+    "type": person_type,
+    "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+
+    headers = {"Content-Type": "application/json"}
+
+    response = requests.post(url, json=payload, headers=headers)
+
+def save_weight(name: str, weight: float, person_type: str):
+    """
+    Calls both save to database and file at the same time. 
+    """
+
+    save_to_file(FILENAME, name, weight, person_type) # Uses filename defined at top of file
+    save_to_database(name, weight, person_type)

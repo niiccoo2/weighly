12fa677cf533e3c3c735a6f8c30215665c548f96
Nico Smith <web@niiccoo2.xyz>
2025-09-04 07:40:03 -0400

    Make load_settings method create file with defualt values if it doesn't exist

--- app/json_utils.py
+++ app/json_utils.py
@@ -5,14 +5,23 @@ default_settings = {
     "SERIALPORT": "/dev/ttyUSB0",
     "BAUDRATE": 9600,
     "theme": "dark-blue",
-    "input_mode": "scale"
+    "input_mode": "manual",
+    "unit": "lbs",
+    "auto_remove_name": True
 }
 
-def load_settings(SETTINGS_FILE = "settings.json"):
+def load_settings(SETTINGS_FILE="settings.json"):
     if os.path.exists(SETTINGS_FILE):
-        with open(SETTINGS_FILE, "r") as f:
-            return json.load(f)
-    return default_settings.copy()
+        try:
+            with open(SETTINGS_FILE, "r") as f:
+                return json.load(f)
+        except (json.JSONDecodeError, OSError):
+            print("Settings file corrupted, resetting to defaults.")
+
+    settings = default_settings.copy()
+    with open(SETTINGS_FILE, "w") as f:
+        json.dump(settings, f, indent=4)
+    return settings
 
 def save_settings(settings, SETTINGS_FILE = "settings.json"):
     with open(SETTINGS_FILE, "w") as f:

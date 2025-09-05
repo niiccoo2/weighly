902ee29bdfbfae8cf58d99b2891c062cc4fdba10
niiccoo2 <nicosmith873@gmail.com>
2025-09-05 09:25:45 -0400

    Start adding settings screen

--- app/settings_screen.py
+++ app/settings_screen.py
@@ -5,19 +5,20 @@ class SettingsScreen(ctk.CTkFrame):
         super().__init__(parent)
         self.controller = controller
 
-        for i in range(4):
+        for i in range(8):
             self.columnconfigure(i, weight=1)
         
         for i in range(6):
             self.rowconfigure(i, weight=1)
         
-
-        self.label = ctk.CTkLabel(self, text="Settings Screen", font=("Helvetica", 40))
-        self.label.grid(row=0, column=1, columnspan=2, rowspan=1)
-
-        self.btn_to_main = ctk.CTkButton(
+        self.btn_back = ctk.CTkButton(
             self, text="Back to Main", 
             command=lambda: controller.show_frame("MainScreen")
         )
-        self.btn_to_main.grid(row=1, column=1, columnspan=2, rowspan=1)
-        
+        self.btn_back.grid(row=0, column=0)
+
+        self.settings_label = ctk.CTkLabel(self, text="Settings", font=("Helvetica", 20))
+        self.settings_label.grid(row=0, column=3)
+
+        self.keep_name_label = ctk.CTkLabel(self, text="Keep name after submission", font=("Helvetica", 20))
+        self.keep_name_label.grid(row=1, column=2)

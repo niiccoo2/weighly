9517ab51bd66b084794bf9515d6c92f7a986e8e0
Nico Smith <web@niiccoo2.xyz>
2025-09-14 10:49:33 -0500

    Add keep_name setting

--- app/json_utils.py
+++ app/json_utils.py
@@ -7,22 +7,26 @@ default_settings = {
     "theme": "dark-blue",
     "input_mode": "manual",
     "unit": "lbs",
-    "auto_remove_name": True
+    "keep_name": True
 }
 
 def load_settings(SETTINGS_FILE="settings.json"):
     if os.path.exists(SETTINGS_FILE):
         try:
             with open(SETTINGS_FILE, "r") as f:
-                return json.load(f)
-        except (json.JSONDecodeError, OSError):
-            print("Settings file corrupted, resetting to defaults.")
+                settings = json.load(f)
+                print(f"Loading {SETTINGS_FILE}:\n{settings}")
+                return settings
+        except (json.JSONDecodeError, OSError) as e:
+            print(f"Settings file corrupted: {e}, resetting to defaults.")
 
     settings = default_settings.copy()
     with open(SETTINGS_FILE, "w") as f:
         json.dump(settings, f, indent=4)
+    print(f"Made defualt file {SETTINGS_FILE}:\n{settings}")
     return settings
 
 def save_settings(settings, SETTINGS_FILE = "settings.json"):
+    print(f"Saving settings as {SETTINGS_FILE}: {settings}")
     with open(SETTINGS_FILE, "w") as f:
         json.dump(settings, f, indent=4)

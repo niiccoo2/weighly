791f48564ba274eb2fb70e79facb76cd51b84e80
niiccoo2 <nicosmith873@gmail.com>
2025-09-28 20:41:34 -0400

    WIP: Add homepage

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,23 +1,11 @@
-<style>
-  /* .bevel-box {
-    padding: 1.5rem;
-    background: #e0e0e0;
-    border-radius: 12px;
-    box-shadow: inset 4px 4px 8px #b8b8b8,
-                inset -4px -4px 8px #ffffff;
-    font-family: sans-serif;
-    text-align: center;
-  } */
-  .lightmode-background {
-	background-color: #F5F5F7;
-	/* background-color: aqua; */
-  }
-  /*.lightmode-foreground {
-	background-color: #ffffff;
-  }*/
+<script lang="ts">
+  import Header from '$lib/Header.svelte';
+  import '$lib/styles_lightmode.css'; // adjust path if you put the CSS somewhere else
+  export let data;
+</script>
 
-</style>
+<Header />
 
-<div class="lightmode-background" style="width: 100vw; height: 100vh;">
-	<p>Weighly Homepage</p>
-</div>
+<main>
+  <slot />
+</main>

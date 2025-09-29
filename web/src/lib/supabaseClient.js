6c277494d82112fc1439ea4a7956dd3d481d876f
niiccoo2 <nicosmith873@gmail.com>
2025-09-29 07:40:49 -0400

    Add login feature

---
+++ web/src/lib/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://oifjrkxhjrtwlrancdho.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9pZmpya3hoanJ0d2xyYW5jZGhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwNjQ4NDUsImV4cCI6MjA3NDY0MDg0NX0.dBUGNaqc6-hcYQzEEUKnwD9gPji6RxqHfRhDeUA6hto'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)

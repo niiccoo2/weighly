0ae4e364a57fcfd35618f9b7146b53caad4b5e7c
Nico Smith <web@niiccoo2.xyz>
2025-10-13 09:33:52 -0400

    Add python success message

---
+++ web/src/routes/python_sign_in_success/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import '$lib/styles_lightmode.css';
+</script>
+
+<svelte:head>
+	<title>Weighly</title>
+</svelte:head>
+
+<main class="flex flex-col items-center mt-8 w-full max-w-4xl mx-auto">
+  <!-- Title centered with button top-right -->
+  <div class="w-full max-w-md p-4 card rounded-xl shadow-lg">
+    <div class="flex flex-col items-center space-y-2">
+      <p class="text-xl font-semibold text-center">Sign In Successful</p>
+      <p class="text-center text-sm">You may close this tab.</p>
+    </div>
+  </div>
+</main>
